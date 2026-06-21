88ce27ac3802ea565b5b975d1761a9a67a4ea9cd
lozhk <numazhu07@gmail.com>
2026-06-21 12:32:54 +1000

    исправил баг

--- src/core/menu.py
+++ src/core/menu.py
@@ -78,10 +78,11 @@ class MainMenu:
         self.selected_index = self.selected_index % len(self.menu_options)
 
     def update_volume(self, event) -> str:
-        if event.key in (pygame.K_LEFT, pygame.K_a):
-            self.volume = max(0, self.volume-10)
-        elif event.key in (pygame.K_RIGHT, pygame.K_d):
-            self.volume = min(100, self.volume+10)
+        if self.state == MenuStates.SETTINGS: 
+            if event.key in (pygame.K_LEFT, pygame.K_a):
+                self.volume = max(0, self.volume-10)
+            elif event.key in (pygame.K_RIGHT, pygame.K_d):
+                self.volume = min(100, self.volume+10)
 
         return cfg.VOLUME_BUTTON
 
